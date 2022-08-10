14d89a75ddd3ffbf449d99c2533efb2b08ad5c82
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-08-10 11:43:20 -0400

    added css and js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const startButton = document.getElementById("start-btn");
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  console.log("Start Quiz");
+}
+
+function nextQuestion() {}
+
+function selectAnswer() {}
+
+function submitAnswer() {}

ff5be6203f16963ac47dd5dbe24d1e24979e6abd
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-08-23 17:00:07 -0400

    rest of code

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const questionsArr = [
     question: "What does JS stand for?",
     options: ["Javascript", "Not Javascript", "Java", "Juicy Slices"],
     answer: "Javascript",
+    id: 1,
   },
   {
     question: "What does CS stand for ",
@@ -14,9 +15,10 @@ const questionsArr = [
       "Javascript",
       "Computer Science",
       "Coffee Supreme",
-      "Juicy Slices",
+      "Computer Stuff",
     ],
     answer: "Computer Science",
+    id: 2,
   },
   {
     question: "What does CS stand for ",
@@ -24,23 +26,47 @@ const questionsArr = [
       "Javascript",
       "Computer Science",
       "Coffee Supreme",
-      "Juicy Slices",
+      "Computer Stuff",
     ],
     answer: "Computer Science",
+    id: 3,
   },
 ];
+let questionIndex = 0;
 
-startBtn.addEventListener("click", function () {
+function startTimer() {
   //add timer
 
+  //timer
   setInterval(function () {
     time--;
     timer.textContent = time;
   }, 1000);
-});
+}
+
+function displayQuestions() {
+  if (questionIndex < questionsArr.length + 1) {
+    console.log(questionsArr[questionIndex]);
+
+    displayAnswers(questionsArr[questionIndex].options);
+  }
+}
+
+function displayAnswers(options) {
+  for (let index = 0; index < options.length; index++) {
+    console.log(options[index]);
+  }
+
+  // if the chosen answer matches the answer in your question object, increase the questionIndex by 1
+}
 
 function nextQuestion() {}
-
 function selectAnswer() {}
-
 function submitAnswer() {}
+
+function startQuiz() {
+  startTimer();
+  displayQuestions();
+}
+
+startBtn.addEventListener("click", startQuiz);
